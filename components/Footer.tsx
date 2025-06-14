c7cde42919424a24929d3890f602436d65665c0b
AvaneeshKarthikeyan <theavi1153@gmail.com>
2025-06-14 23:12:41 +0530

    Changes in Navbar, Clickspark, removed twitter icon and some css

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,19 +33,17 @@ const Footer = () => {
         </a>
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-        <p className="md:text-base text-sm md:font-normal font-light">
-         
-        </p>
+        <p className="md:text-base text-sm md:font-normal font-light"></p>
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
-            <a href={info.link}>
-            <div
-              key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-            >
-              <img src={info.img} alt="icons" width={20} height={20} />
-            </div>
+            <a key={info.id} href={info.link}>
+              <div
+                key={info.id}
+                className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              >
+                <img src={info.img} alt="icons" width={20} height={20} />
+              </div>
             </a>
           ))}
         </div>
